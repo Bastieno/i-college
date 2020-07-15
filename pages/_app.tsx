3fbcd614da67400bc3db1b5da2151e7b9066ffaf
Francis Nduamaka <francis.nduamaka@gmail.com>
2020-07-15 09:27:54 +0100

    adding nprogress, and fixing _document.tsx to use evergreen-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import { AppProps } from 'next/app';
+import Router from 'next/router';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+import NProgress from 'nprogress';
 import { GlobalStyle } from '../styles/globalStyles';
 import { getTheme } from '../utils';
 import AppContext from '../contexts/appContext';
+import { version } from '../package.json';
 import 'antd/dist/antd.css';
+
+const showProgressBar = (delay: number) => {
+  const timer = setTimeout(() => NProgress.start(), delay);
+  Router.events.on('routeChangeComplete', () => stopProgress(timer));
+  Router.events.on('routeChangeError', () => stopProgress(timer));
+};
+
+const stopProgress = (timer: number) => {
+  clearTimeout(timer);
+  NProgress.done();
+};
+
+Router.events.on('routeChangeStart', () => showProgressBar(300));
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const modes: string[] = ['light', 'dark'];
@@ -20,6 +37,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <AppContext.Provider value={{ toggleMode, mode }}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Head>
+          <title>{`iCollege v${version}`}</title>
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </AppContext.Provider>
